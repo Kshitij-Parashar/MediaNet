be6f28e516450e8b6c93729583dec0c3a7079feb
socomo <you@example.com>
2018-03-18 18:35:31 +0530

    book added to favourites will not be removed from book list

--- js/controllers/showBooksController.js
+++ js/controllers/showBooksController.js
@@ -30,15 +30,22 @@ MediaNetApp.controller('showBooksController',function ($http, $scope,$rootScope)
         $scope.favList = JSON.parse(localStorage.getItem('favList'));
     }
     $scope.deleteBook = function(index){
+        for(var i = 0; i < $scope.favList.length; i++) {
+            if ($scope.favList[i].bookName == $scope.bookList[index].bookName && $scope.favList[i].authorName == $scope.bookList[index].authorName && $scope.favList[i].bookPrice == $scope.bookList[index].bookPrice) {
+                $scope.favList.splice(i,1)
+                break;
+            }
+        }
         $scope.bookList.splice(index,1);
         $scope.oldBookList = $scope.bookList;
         localStorage.setItem('bookList',JSON.stringify($scope.bookList));
+        localStorage.setItem('favList',JSON.stringify($scope.favList));
         $rootScope.emitCount();
     };
     $scope.addToFav = function(index){
         $scope.favList.push($scope.bookList[index]);
         localStorage.setItem('favList',JSON.stringify($scope.favList));
-        $scope.bookList.splice(index,1);
+        //$scope.bookList.splice(index,1);
         localStorage.setItem('bookList',JSON.stringify($scope.bookList));
         $scope.oldBookList = $scope.bookList;
         $rootScope.emitCount();
