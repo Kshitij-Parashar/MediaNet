e9e977e39ff999ae732cb5237035a8edea2dda2b
socomo <you@example.com>
2018-03-18 18:19:40 +0530

    search

--- js/controllers/favouriteBooksController.js
+++ js/controllers/favouriteBooksController.js
@@ -4,17 +4,36 @@
 MediaNetApp.controller('favoriteBooksController',function ($http, $scope,$rootScope) {
     $scope.bookList = [];
     $scope.favList = [];
+    $scope.oldFavList = [];
+
+    $scope.$watch('bookSearch',function(newVal,oldVal){
+        var tempbookList = [];
+        if(newVal.trim() == ''){
+            $scope.favList = $scope.oldFavList;
+            return;
+        }
+        if(newVal != oldVal) {
+            $scope.favList.filter(function (obj) {
+                if(obj.bookName.indexOf(newVal) != -1){
+                    tempbookList.push(obj);
+                }
+            });
+        }
+        $scope.favList = tempbookList;
+    })
     if(localStorage.getItem('bookList')){
         $scope.bookList = JSON.parse(localStorage.getItem('bookList'));
     }
     if(localStorage.getItem('favList')){
         $scope.favList = JSON.parse(localStorage.getItem('favList'));
+        $scope.oldFavList =  $scope.favList;
     }
     $scope.removeBook = function(index){
         $scope.bookList.push($scope.favList[index]);
         localStorage.setItem('bookList',JSON.stringify($scope.bookList));
         $scope.favList.splice(index,1);
         localStorage.setItem('favList',JSON.stringify($scope.favList));
+        $scope.oldFavList = $scope.favList;
         $rootScope.emitCount();
     };
 
--- js/controllers/showBooksController.js
+++ js/controllers/showBooksController.js
@@ -3,15 +3,35 @@
  */
 MediaNetApp.controller('showBooksController',function ($http, $scope,$rootScope) {
     $scope.bookList = [];
+    $scope.oldBookList = [];
     $scope.favList = [];
+    $scope.bookSearch = ""
+
+    $scope.$watch('bookSearch',function(newVal,oldVal){
+        var tempbookList = [];
+        if(newVal.trim() == ''){
+            $scope.bookList = $scope.oldBookList;
+            return;
+        }
+        if(newVal != oldVal) {
+            $scope.bookList.filter(function (obj) {
+                if(obj.bookName.indexOf(newVal) != -1){
+                    tempbookList.push(obj);
+                }
+            });
+        }
+        $scope.bookList = tempbookList;
+    })
     if(localStorage.getItem('bookList')){
         $scope.bookList = JSON.parse(localStorage.getItem('bookList'));
+        $scope.oldBookList = JSON.parse(localStorage.getItem('bookList'));
     }
     if(localStorage.getItem('favList')){
         $scope.favList = JSON.parse(localStorage.getItem('favList'));
     }
     $scope.deleteBook = function(index){
         $scope.bookList.splice(index,1);
+        $scope.oldBookList = $scope.bookList;
         localStorage.setItem('bookList',JSON.stringify($scope.bookList));
         $rootScope.emitCount();
     };
@@ -20,6 +40,7 @@ MediaNetApp.controller('showBooksController',function ($http, $scope,$rootScope)
         localStorage.setItem('favList',JSON.stringify($scope.favList));
         $scope.bookList.splice(index,1);
         localStorage.setItem('bookList',JSON.stringify($scope.bookList));
+        $scope.oldBookList = $scope.bookList;
         $rootScope.emitCount();
     }
 
